afa279b0847b10b679ec2279c22a6436e77d9bf4
mishra_abhi <abhi200108010@gmail.com>
2022-09-09 19:13:17 +0530

    Added Google SignIn

--- app/src/main/java/com/hbtu/cycleapp/MainActivity.java
+++ app/src/main/java/com/hbtu/cycleapp/MainActivity.java
@@ -1,14 +1,74 @@
 package com.hbtu.cycleapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.hbtu.cycleapp.auth.GoogleSignInActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    Button logout;
+    GoogleSignInClient gsc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        logout = findViewById(R.id.btnLogout);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(MainActivity.this, gso);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(currentUser!=null) {
+                    gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                mAuth.signOut();
+                                Intent intent = new Intent(MainActivity.this, GoogleSignInActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+                }
+            }
+        });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        currentUser = mAuth.getCurrentUser();
+        if(currentUser == null){
+            Intent intent = new Intent(MainActivity.this, GoogleSignInActivity.class);
+            startActivity(intent);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/hbtu/cycleapp/auth/GoogleSignInActivity.java
@@ -0,0 +1,126 @@
+package com.hbtu.cycleapp.auth;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.hbtu.cycleapp.MainActivity;
+import com.hbtu.cycleapp.R;
+
+import java.util.Objects;
+
+public class GoogleSignInActivity extends AppCompatActivity {
+
+    public static final int RC_SIGN_IN = 100;
+    GoogleSignInClient gsc;
+    FirebaseAuth mAuth;
+
+    Button btnSignIn;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_sign_in);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnSignIn = findViewById(R.id.btnLogin);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                progressDialog = new ProgressDialog(GoogleSignInActivity.this);
+                progressDialog.setMessage("Google Sign In..");
+                progressDialog.show();
+
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
+
+                gsc = GoogleSignIn.getClient(GoogleSignInActivity.this, gso);
+                gsc.revokeAccess();
+
+                Intent signInIntent = gsc.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+
+            try{
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if(account!= null){
+                    String email = account.getEmail();
+                    if(Objects.equals(email, "200108010@hbtu.ac.in")){
+                        firebaseAuthWithGoogle(account.getIdToken());
+                    }
+                    else{
+                        progressDialog.dismiss();
+                        gsc.signOut();
+                        finish();
+                        Toast.makeText(GoogleSignInActivity.this, "Please use your college mail-id", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            catch (ApiException e){
+                progressDialog.dismiss();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+
+                            progressDialog.dismiss();
+//                            String currentUser = mAuth.getCurrentUser().getUid();
+//                            updateUI(currentUser);
+                            Intent intent = new Intent(GoogleSignInActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                        }
+                        else{
+                            progressDialog.dismiss();
+                            Toast.makeText(GoogleSignInActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    }
+                });
+    }
+
+}
